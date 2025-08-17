b5399864a85191dc0b6effcd19816be7ac032ad4
kqcoxn <mjy03730@163.com>
2025-08-17 13:54:27 +0800

    feat: 边标签

--- src/components/flow/edges.tsx
+++ src/components/flow/edges.tsx
@@ -1,13 +1,18 @@
 import style from "../../styles/edges.module.less";
 
 import { memo } from "react";
-import { BaseEdge, getBezierPath, type EdgeProps } from "@xyflow/react";
+import {
+  BaseEdge,
+  getBezierPath,
+  EdgeLabelRenderer,
+  type EdgeProps,
+} from "@xyflow/react";
 import classNames from "classnames";
 
 import { SourceHandleTypeEnum } from "./nodes";
 
 function MarkedEdge(props: EdgeProps) {
-  const [edgePath] = getBezierPath({ ...props });
+  const [edgePath, labelX, labelY] = getBezierPath({ ...props });
   let markClass = "";
   if (props.selected) {
     markClass = style["edge-selected"];
@@ -25,12 +30,31 @@ function MarkedEdge(props: EdgeProps) {
     }
   }
 
-  return (
+  const Edge = (
     <BaseEdge
       className={classNames(style.edge, markClass)}
       id={props.id}
       path={edgePath}
     />
+  );
+
+  return false ? (
+    <>
+      {Edge}
+      <EdgeLabelRenderer>
+        <div
+          className={style.label}
+          style={{
+            position: "absolute",
+            transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
+          }}
+        >
+          {/* {props.label} */}1
+        </div>
+      </EdgeLabelRenderer>
+    </>
+  ) : (
+    Edge
   );
 }
 
