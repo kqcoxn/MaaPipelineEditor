4bad1b7df1a2a2b0049a80bf7cab984f84186006
kqcoxn <mjy03730@163.com>
2025-08-31 22:43:02 +0800

    test: 测试生产环境按键重映射

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import style from "./styles/App.module.less";
 
-import { useEffect, memo } from "react";
+import { useEffect } from "react";
 import { Flex, Layout, Splitter, message } from "antd";
 const { Header: HeaderSection, Content } = Layout;
 
@@ -15,6 +15,26 @@ import FilePanel from "./components/panels/FilePanel";
 import ConfigPanel from "./components/panels/ConfigPanel";
 import ErrorPanel from "./components/panels/ErrorPanel";
 
+// 按键重定向
+function keyRedirection() {
+  document.addEventListener("keydown", function (event) {
+    console.log("注册按键重定向");
+    if (event.key === "Delete") {
+      const backspaceEvent = new KeyboardEvent("keydown", {
+        key: "Backspace",
+        code: "Backspace",
+        keyCode: 8,
+        which: 8,
+        bubbles: true,
+        cancelable: true,
+      });
+
+      console.log("触发按键");
+      event.target?.dispatchEvent(backspaceEvent);
+    }
+  });
+}
+
 /**主程序 */
 function App() {
   // onMounted
@@ -24,20 +44,7 @@ function App() {
     if (!err) message.success("已读取本地缓存");
 
     // 按键重定向
-    document.addEventListener("keydown", function (event) {
-      if (event.key === "Delete") {
-        event.preventDefault();
-        const backspaceEvent = new KeyboardEvent("keydown", {
-          key: "Backspace",
-          code: "Backspace",
-          keyCode: 8,
-          which: 8,
-          bubbles: true,
-          cancelable: true,
-        });
-        event.target?.dispatchEvent(backspaceEvent);
-      }
-    });
+    keyRedirection();
   }, []);
 
   // 渲染组件
