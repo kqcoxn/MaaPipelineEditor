31b25c8ffa56f3cb3bef8977bb09aeb834c17e20
kqcoxn <mjy03730@163.com>
2025-12-06 23:29:11 +0800

    refactor: 删除旧版 server

--- server/core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""WebSocket 服务器核心模块"""
-
-from .websocket_server import WebSocketTestServer
-from .handlers import MessageHandler
-
-__all__ = ["WebSocketTestServer", "MessageHandler"]
--- server/ui/__init__.py
+++
@@ -1,5 +0,0 @@
-"""UI 界面模块"""
-
-from .server_ui import ServerUI
-
-__all__ = ["ServerUI"]
